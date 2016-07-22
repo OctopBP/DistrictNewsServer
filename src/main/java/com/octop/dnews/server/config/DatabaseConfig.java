94834141e557c428ca51e0fb68ab796605811fdb
Boris Proshin <kaktusao@mail.ru>
2016-07-22 16:34:29 +0300

    Подготовка к работе с БД

---
+++ src/main/java/com/octop/dnews/server/config/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package com.octop.dnews.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("com.octop.dnews.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.octop.dnews.server")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment env;
+
+}
