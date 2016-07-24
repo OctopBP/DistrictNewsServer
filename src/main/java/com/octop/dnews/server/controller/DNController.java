5e8ce8f68a27a1368b2bd439a57bc1b03317c179
Boris Proshin <kaktusao@mail.ru>
2016-07-24 20:20:47 +0300

    Создание простого REST сервиса

--- src/main/java/com/octop/dnews/server/controller/DNController.java
+++ src/main/java/com/octop/dnews/server/controller/DNController.java
@@ -1,18 +1,29 @@
 package com.octop.dnews.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.octop.dnews.server.entity.News;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/news")
 public class DNController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getNews(ModelMap model){
-        return "My news";
+    public News getNews(){
+        return createMockNews();
+    }
+
+    private News createMockNews() {
+        News news = new News();
+        news.setId(1);
+        news.setTitle("Good news, evreone");
+        news.setNewsDate(new Date());
+
+        return news;
     }
 }
