98d78029ab4aa15388de8bb8470c81fb0a5b5177
Boris Proshin <kaktusao@mail.ru>
2016-07-24 19:41:03 +0300

    Конфигурация Entity и создание таблицы в БД

---
+++ src/main/java/com/octop/dnews/server/entity/News.java
@@ -0,0 +1,49 @@
+package com.octop.dnews.server.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "news")
+public class News {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "newsData", nullable = false)
+    private Date newsDate;
+
+    public News() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getNewsDate() {
+        return newsDate;
+    }
+
+    public void setNewsDate(Date newsDate) {
+        this.newsDate = newsDate;
+    }
+}
